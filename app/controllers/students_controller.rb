a461c0b04004d04c829eeb8bb3f7848690252434
Mark Delgado <markdelgado.md@hotmail.com>
2022-08-29 16:30:22 -0400

    commit tree 33ca8179c469490f972f3a661a2f870c890d75d5

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
-  def index
-    students = Student.all
+ def index
+    students = if params[:name]
+                 Student.by_name(params[:name])
+               else
+                 Student.all
+               end    
     render json: students
   end
+
+  def id
+    student =Student.find(params[:id])
+    render json: student
+  
+  end
+
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,15 @@
 class Student < ApplicationRecord
 
+  def self.by_name (name)
+    Student.where("first_name LIKE ? OR last_name LIKE?", "%#{name}%", "%#{name}%")
+
+  end
   def to_s
     "#{first_name} #{last_name}"
+  end
+
+  def full_name
+
   end
 
 end
